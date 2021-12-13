5f697d83566d28fc54533bd3dd1807686dd39542
Simon Lenz <simon13lenz@gmail.com>
2021-12-13 10:29:35 +0100

    implement Director

--- Woche 2/ConcreteBuilder1.py
+++ Woche 2/ConcreteBuilder1.py
@@ -71,6 +71,28 @@ if __name__ == "__main__":
     builder object.
     """
 
+
+class Director:
+    def __init__(self) -> None:
+        self._builder = None
+
+    @property
+    def builder(self) -> Builder:
+        return self._builder
+
+    @builder.setter
+    def builder(self, builder: Builder) -> None:
+        self._builder = builder
+
+    def build_minimal_viable_product(self) -> None:
+        self._builder.produce_part_a()
+
+    def build_full_featured_product(self) -> None:
+        self.builder.produce_part_a()
+        self.builder.produce_part_b()
+        self.builder.produce_part_c()
+
+
 c = ConcreteBuilder1()
 c.produce_part_a()
 c.produce_part_b()
